118b76e106c7a38965adef242c73156353e8d024
Alfi-99 <trsvalfi@gmail.com>
2026-04-10 11:20:38 +0700

    update lagi

--- db_tier.tf
+++ db_tier.tf
@@ -1,9 +1,18 @@
-# RDS Tier
 resource "aws_db_subnet_group" "main" {
   name       = "main-db-subnet-group"
-  subnet_ids = aws_subnet.pub[*].id
+  subnet_ids = aws_subnet.pub[*].id # Ini manggul subnet dari network.tf
+
+  tags = {
+    Name = "Main DB Subnet Group"
+  }
 }
 
+# 1. Security Group untuk Database
+resource "aws_security_group" "db_sg" {
+  name   = "db-sg"
+  vpc_id = aws_vpc.main.id
+
+  # Aturan masuk: Cuma boleh dari ECS dan Bastion
   ingress {
     from_port       = 3306
     to_port         = 3306
@@ -11,16 +20,24 @@ resource "aws_db_subnet_group" "main" {
     security_groups = [aws_security_group.ecs_sg.id, aws_security_group.bastion_sg.id]
   }
 
+  # Aturan keluar: Bebas
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 2. Database Instance-nya
 resource "aws_db_instance" "terradb" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  instance_class       = "db.t3.micro"
-  db_name              = "terradb"
-  username             = "admin"
-  password             = "admin12345"
-  multi_az             = true
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  skip_final_snapshot  = true
-  publicly_accessible  = false
+  allocated_storage      = 20
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  db_name                = "terradb"
+  username               = "admin"
+  password               = "admin12345"
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id] # Panggil SG di atas
+  skip_final_snapshot    = true
 }
--- network.tf
+++ network.tf
@@ -62,3 +62,19 @@ resource "aws_instance" "bastion" {
 
   tags = { Name = "Bastion-Host" }
 }
+
+# Tambahkan ini di network.tf agar Bastion tahu harus pakai OS apa
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # ID milik Canonical (Pembuat Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
